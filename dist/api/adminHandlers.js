031b4548891cf99b47a101af8bc07e1a7dec8fcc
bwalla796 <bwalla796@gmail.com>
2025-12-30 13:35:43 -0500

    Stopped tracking dist dir

--- dist/api/adminHandlers.js
+++
@@ -1,9 +0,0 @@
-import { deleteTask, getTasks } from "../db/tasks.js";
-export async function handlerStats() {
-    const tasks = await getTasks();
-    return (Array.isArray(tasks)) ? tasks.length : 0;
-}
-export async function handlerResetTasks() {
-    deleteTask();
-    console.log("All tasks have been reset.");
-}
